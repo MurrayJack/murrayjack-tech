7c3d36b30dc7d43ec0ecab59aa0a5df401cfe978
Murray Jack <MJack@starrez.com>
2019-08-25 17:44:11 +1000

    thank you page

--- app/src/pages/thank-you.js
+++ app/src/pages/thank-you.js
@@ -1,10 +1,33 @@
 import React from "react"
+import styled from "styled-components";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+const Page = styled.div`
+    background-color: #2f3c4f;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    
+    > main {
+        display: inline-block;
+        background-color: white;        
+        position: fixed;
+        top: 30%;
+        left: calc(50% - 200px);
+        width: 400px;
+        box-sizing: border-box;
+        padding: 20px;
+        border-radius: 5px;
+    }
+`;
 
 const NotFoundPage = () => (
-  <div>Thank you!</div>
+    <Page>
+        <main>
+            Thank you!
+      </main>
+    </Page>
 )
 
 export default NotFoundPage
