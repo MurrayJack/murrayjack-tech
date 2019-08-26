20525e97aac80851d82fda2034f594d4bf534f2b
Murray Jack <MJack@starrez.com>
2019-08-26 17:52:51 +1000

    added a site to the bottom of this page

--- app/src/components/footer.jsx
+++ app/src/components/footer.jsx
@@ -1,10 +1,34 @@
 import React from "react"
-import Container from "./container";
+import styled from "styled-components"
 
+const Bar = styled.footer`
+    color: white;
+    background-color: #2f3c4f;
+    padding-bottom: 120px;
+`;
+
+const Section = styled.section`
+    max-width: 100%;
+    margin: 0 auto;
+    padding: 1.45rem 20px;
+    display: grid;
+    grid-template-columns: 50% 50%;
+
+    a {
+        color: white;
+    }
+
+    @media (min-width: 960px) {
+        max-width: 960px;
+        padding: 1.45rem 0;
+    }
+`;
 
 export default () => (
-    <Container Color="white" BGColor="#2f3c4f">
-        Created Using Gatsby | Sanity.io | Netlify
-    </Container>
+    <Bar>
+        <Section>
+            <div>Site Created Using: <a href="https://www.gatsbyjs.org/">Gatsby</a> | <a href="https://www.sanity.io/">Sanity.IO</a> | <a href="https://app.netlify.com">Netflify</a></div>
+        </Section>
+    </Bar>
 )
 
