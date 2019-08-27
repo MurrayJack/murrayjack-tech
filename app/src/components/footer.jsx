5b1d2e802ec5fe94b552a4444be09ec2dc1c7d4f
Murray Jack <MJack@starrez.com>
2019-08-27 17:38:44 +1000

    added link to footer

--- app/src/components/footer.jsx
+++ app/src/components/footer.jsx
@@ -28,6 +28,7 @@ export default () => (
     <Bar>
         <Section>
             <div>Site Created Using: <a href="https://www.gatsbyjs.org/">Gatsby</a> | <a href="https://www.sanity.io/">Sanity.IO</a> | <a href="https://app.netlify.com">Netflify</a></div>
+            <div><a target="_new" href="https://www.notion.so/http-murrayjack-tech-60dd1a8964ed4563a030baf2410adbc4">Development Notes</a></div>
         </Section>
     </Bar>
 )
