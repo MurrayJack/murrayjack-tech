1530b6fa80fa5d2b1700b60a77d0c784c4aec271
Murray Jack <mjack@starrez.com>
2020-05-21 19:36:00 +1000

    slight change

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -5,7 +5,11 @@ import "../../static/normalize.css";
 export default () => <div>
     <div style={{ display: "grid", alignItems: "center", justifyItems: "center", height: "100vh", gridGap: "24px", gridTemplateRows: "100px 1fr 1fr", background: "black", color: "white" }}>
         <div style={{ color: "white", fontSize: "1.6em" }} href="./resume">Murray Jack </div>
-        <div><a style={{ color: "white", fontSize: "1.6em" }} href="./resume">Resume</a></div>
-        <div><a style={{ color: "white", fontSize: "1.6em" }} href="./blog">Blog</a></div>
+        <div>
+            <a style={{ color: "white", fontSize: "1.6em" }} href="./resume">Resume</a>
+            <span> | </span>
+            <a style={{ color: "white", fontSize: "1.6em" }} href="./blog">Blog</a>
+        </div>
+        <div></div>
     </div>
 </div>
