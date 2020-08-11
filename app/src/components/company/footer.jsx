7d10cb8a37e6d8f46c75f6e800f61b71c3fa7375
Murray Jack <murrayjack@gmail.com>
2020-08-11 17:08:43 +1000

    latest

--- app/src/components/company/footer.jsx
+++ app/src/components/company/footer.jsx
@@ -4,19 +4,28 @@ export default () => (
     <>
         <footer>
             <section>
-                <span>Site Created Using</span>
-                <a href="https://www.gatsbyjs.org/">Gatsby</a>
-                <a href="https://www.sanity.io/">Sanity.IO</a>
-                <a href="https://app.netlify.com">Netflify</a>
+                <p>
+                    <span>Site Created Using</span>
+                    <a href="https://www.gatsbyjs.org/">Gatsby</a>
+                    <a href="https://www.sanity.io/">Sanity.IO</a>
+                    <a href="https://app.netlify.com">Netflify</a>
+                    <span>❤️</span>
+                </p>
             </section>
         </footer>
 
         <style jsx>{`
+            p {
+                display: grid;
+                grid-auto-flow: column;
+                grid-gap: var(--gaps-small)
+            }
+
             footer {
                 padding-bottom: 120px;
                 font-size: var(--font-small);
             }
-        
+
             section {
                 max-width: 100%;
                 margin: 0 auto;
