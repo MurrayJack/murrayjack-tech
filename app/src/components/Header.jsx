377b45b97a77d4091aa35a97a639abcb03850188
Murray Jack <mjack@starrez.com>
2020-11-11 16:18:39 +1100

    sluight colour cvhanger

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -83,9 +83,6 @@ export const Header = ({ name }) => {
                         ))}
                     </AnimateSharedLayout>
                 </div>
-                <div>
-                    {/* <Hamburger size={20} toggled={isOpen} toggle={onClick} /> */}
-                </div>
             </header>
 
             <style jsx>{`
@@ -100,7 +97,6 @@ export const Header = ({ name }) => {
                     background: -webkit-linear-gradient(to right, #61045F, #AA076B);  /* Chrome 10-25, Safari 5.1-6 */
                     background: linear-gradient(to right, #61045F, #AA076B); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
                     
-                
                 }
 
                 .thingame {
