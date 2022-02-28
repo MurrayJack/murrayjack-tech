8e16d1fabbcb14630468c88151f10432a0423d31
Murray Jack <mjack@starrez.com>
2022-02-28 13:19:14 +1100

    fasttech

---
+++ quickfunc.tech/gatsby-config.js
@@ -0,0 +1,45 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+    siteMetadata: {
+        title: ``,
+        siteUrl: `https://www.yourdomain.tld`,
+    },
+    plugins: [
+        {
+            resolve: 'gatsby-source-sanity',
+            options: {
+                projectId: 'bdj4frfl',
+                dataset: 'production',
+            },
+        },
+        'gatsby-plugin-styled-components',
+        'gatsby-plugin-image',
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sitemap',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                icon: 'src/images/icon.png',
+            },
+        },
+        'gatsby-plugin-mdx',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: './src/pages/',
+            },
+            __key: 'pages',
+        },
+    ],
+};
