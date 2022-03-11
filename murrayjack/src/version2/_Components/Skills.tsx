06fb50be137b1755f81d049507886efee4a8ef9b
Murray Jack <mjack@starrez.com>
2022-03-11 18:34:38 +0000

    latest

--- murrayjack/src/version2/_Components/Skills.tsx
+++ murrayjack/src/version2/_Components/Skills.tsx
@@ -5,19 +5,34 @@ import { Section } from './Section';
 export const Skills = () => {
     const { keySkills } = useData().allSanityPersonalDetails.nodes[0];
     return (
-        <Section caption="Skills">
-            {keySkills.map((skill) => (
-                <ul>
-                    <li>{skill.name}</li>
-                    <li>
-                        <ul className="list">
-                            {skill.detail.map((detail) => (
-                                <li>{detail}</li>
-                            ))}
-                        </ul>
-                    </li>
-                </ul>
-            ))}
-        </Section>
+        <>
+            <Section caption="Skills">
+                {keySkills.map((skill) => (
+                    <ul>
+                        <li>{skill.name}</li>
+                        <li>
+                            <ul className="list">
+                                {skill.detail.map((detail) => (
+                                    <li>{detail}</li>
+                                ))}
+                            </ul>
+                        </li>
+                    </ul>
+                ))}
+            </Section>
+            <style jsx>{`
+                ul {
+                    list-style: none;
+                }
+
+                ul li ul li {
+                    padding-left: 16px;
+                }
+
+                ul li ul li:last-of-type {
+                    padding-bottom: 16px;
+                }
+            `}</style>
+        </>
     );
 };
